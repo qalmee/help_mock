5080adedc8615ee1d68788db5df36bd6aca01399
qalmee <komarevtsev24@gmail.com>
2019-04-20 00:10:38 +0700

    123

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, request
+from flask import Flask, render_template, request, redirect, request, Response
 import jinja2
 import os
 import questions
@@ -7,6 +7,19 @@ import answers
 from movesfinder import MovesFinder
 
 app = Flask(__name__)
+
+
+#Read JSON data into the datastore variable
+json_var = json.load(open("mock.json", 'r'))
+
+#Use the new datastore datastructure
+
+keys = json_var.keys()
+keys_arr = []
+for key in keys:
+	keys_arr.append(key)
+pos = -1
+
 
 @app.route('/')
 def hello():
@@ -24,6 +37,14 @@ def post():
 
 @app.route('/get_help', methods=['POST'])
 def get_help():
+	global pos
+	global json_var
+	global keys_arr
+	pos = pos + 1
+	return str(json_var[keys_arr[pos]]).encode("cp1251")
+
+@app.route('/get_help_test', methods=['POST'])
+def get_help_test():
 	json = request.get_json()
 	movesfinder = MovesFinder()
 	match = questions.find_best_match(json["question"])
@@ -32,8 +53,9 @@ def get_help():
 		possible_moves = movesfinder.get_list_moves(json["board"])
 	elif match[0] == 1:
 		possible_moves = movesfinder.how_best_move(json["board"])
-	answer = ""	
-	return [answer, possible_moves]
+	answer = "haha"	
+	print(possible_moves)
+	return ("123", "234")
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
