3af798261aadbf2dcebc478207d18ee798084e14
SilenceIX <PM72.KIM@outlook.com>
2019-04-19 23:43:10 +0700

    Change board to string

--- movesfinder.py
+++ movesfinder.py
@@ -11,17 +11,25 @@ class MovesFinder:
         self._info_handler=chess.uci.InfoHandler()
         self._engine.info_handlers.append(self._info_handler)
 
-    def get_list_moves(self, board):
+    def get_list_moves(self, sboard):
+        board = chess.Board(sboard)        
         self._engine.position(board)
         reslist = list()
         moves = board.legal_moves
+        print(board)
         for move in moves:
-            self._engine.go(searchmoves=[move], depth=5)
+            self._engine.go(searchmoves=[move], depth=10)
             scr = self._info_handler.info['score'][1].cp
-            scrf = round(float(scr)/1000, 2)
+            mate = self._info_handler.info['score'][1].mate
+            if mate == 1:
+                scrf = 5
+            elif mate == -1:
+                continue 
+            elif scr != None:
+                scrf = round(float(scr)/1000, 2)
             reslist.append({'move' : move.uci(), 'score' : scrf})
         reslist.sort(key=lambda p:-p['score'])
         return reslist
     
-    def how_best_move(self, board):
-        return self.get_list_moves(board)[0]
+    def how_best_move(self, sboard):
+        return self.get_list_moves(sboard)[0]
